24bddea66c2d4c1e56fe8876c344b262ca18c727
Jingjing Jiang <jing.jing.jiang@outlook.com>
2024-10-08 17:57:17 +0200

    init

---
+++ calcualte_mua_from_chromophores/demo.m
@@ -0,0 +1,6 @@
+c_HHb = 1e-5 % numbers here dont mean anything, just to show how to function
+c_OHb = 1e-4
+c_H2O = 0.7
+c_Lipid = 0.01
+wav = 700 % nm
+mua = get_mua(c_HHb, c_OHb, c_H2O, c_Lipid,  wav)
